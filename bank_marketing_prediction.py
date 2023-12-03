5f0ee3e1080210b1b3c67f0bba8221b400be7ffb
madhav28 <madhav2800@gmail.com>
2023-12-03 08:43:36 -0500

    removed unused packages

--- bank_marketing_prediction.py
+++ bank_marketing_prediction.py
@@ -1,15 +1,5 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import seaborn as sns
-import plotly.express as px
-import altair as alt
-import hiplot as hip
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import LabelEncoder
 
 st.title("Bank Marketing Analysis")
 
