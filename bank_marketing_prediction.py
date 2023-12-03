7722ae652771e69d3cffeb90c45a51cf22a4c0a5
madhav28 <madhav2800@gmail.com>
2023-12-02 20:40:46 -0500

    Changed link for the raw data

--- bank_marketing_prediction.py
+++ bank_marketing_prediction.py
@@ -17,5 +17,5 @@ tab1, tab2, tab3, tab4, tab5 = st.tabs(
     ['Introduction', 'Objectives', 'Data Visualisation / Analysis', 'Predictive Modelling', 'Conclusion'])
 
 bank_marketing_df = pd.read_csv(
-    "https://raw.githubusercontent.com/madhav28/Bank-Marketing-Analysis/main/bank-full.csv", sep=';')
+    "https://raw.githubusercontent.com/madhav28/Bank-Marketing-Prediction/master/bank-full.csv", sep=';')
 bank_marketing_df.rename(columns={'y': 'outcome'},  inplace=True)
